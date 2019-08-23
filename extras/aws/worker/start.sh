bafcc739bb9c824ea28751e31f76bc04286f2f04
Armando Uch <jahrmando@gmail.com>
2019-08-23 11:13:47 -0500

    test 3

--- extras/aws/worker/start.sh
+++ extras/aws/worker/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /etc/profile.d/env_sidekiq.sh
 
-# cd /opt/sidekiq && bundle exec sidekiq \
-#   --logfile=log/sidekiq-$(hostname).log \
-#   --pidfile=tmp/sidekiq-$(hostname).pid \
-#   --config /opt/sidekiq/config/sidekiq.yml \
-#   --daemon
+cd /opt/sidekiq && bundle exec sidekiq \
+  --logfile=log/sidekiq-$(hostname).log \
+  --pidfile=tmp/sidekiq-$(hostname).pid \
+  --config /opt/sidekiq/config/sidekiq.yml \
+  --daemon
